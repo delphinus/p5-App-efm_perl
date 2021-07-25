fd4718699d0da29ba8e62c22322c33690716f30c
delphinus <me@delphinus.dev>
2021-07-25 09:32:53 +0900

    Add first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::efm\_perl - It's new $module
+
+# SYNOPSIS
+
+    use App::efm_perl;
+
+# DESCRIPTION
+
+App::efm\_perl is ...
+
+# LICENSE
+
+Copyright (C) delphinus.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+delphinus <me@delphinus.dev>
---
+++ lib/App/efm_perl.pm
@@ -0,0 +1,39 @@
+package App::efm_perl;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::efm_perl - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::efm_perl;
+
+=head1 DESCRIPTION
+
+App::efm_perl is ...
+
+=head1 LICENSE
+
+Copyright (C) delphinus.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+delphinus E<lt>me@delphinus.devE<gt>
+
+=cut
+
